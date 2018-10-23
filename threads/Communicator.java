02a58e6ad3aa73ec51e433706dec90f7b51d1a87
Jadon Hansell <alcamore@gmail.com>
2018-10-23 16:43:48 -0700

    Implement better speaker/listener signaling
    Passws 2/3 test cases

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -1,6 +1,13 @@
 package nachos.threads;
 
 import nachos.machine.*;
+import nachos.threads.Condition;
+import nachos.threads.Lock;
+import nachos.threads.KThread;
+
+import java.util.ArrayList;
+
+import java.util.LinkedList;
 
 /**
  * A <i>communicator</i> allows threads to synchronously exchange 32-bit
@@ -10,10 +17,29 @@ import nachos.machine.*;
  * be paired off at this point.
  */
 public class Communicator {
+
+    private Lock mutex;
+    
+    private Condition okToListen;
+    private int waitingListeners = 0;
+
+    private Condition okToSpeak;
+    private int waitingSpeakers = 0;
+
+
+    private Condition messageReady;
+    private int currentMessage = 0;
+
+    private boolean validMessage = false;
+
     /**
      * Allocate a new communicator.
      */
     public Communicator() {
+        this.mutex = new Lock();
+        this.okToListen = new Condition(mutex);
+        this.okToSpeak = new Condition(mutex);
+        this.messageReady = new Condition(mutex);
     }
 
     /**
@@ -27,6 +53,24 @@ public class Communicator {
      * @param word the integer to transfer.
      */
     public void speak(int word) {
+        mutex.acquire();
+        waitingSpeakers++;
+
+        if (waitingListeners == 0) {
+            okToSpeak.sleep();
+        }
+
+        if (!mutex.isHeldByCurrentThread()) {
+            mutex.acquire();
+        }
+
+        currentMessage = word;
+        validMessage = true;
+        waitingSpeakers--;
+        
+        messageReady.wake();
+
+        mutex.release();
     }
 
     /**
@@ -36,6 +80,27 @@ public class Communicator {
      * @return the integer transferred.
      */
     public int listen() {
-        return 0;
+        mutex.acquire();
+        waitingListeners++;
+        
+        while (waitingSpeakers == 0) {
+            okToListen.sleep();
+        }
+        
+        while(!validMessage) {
+            okToSpeak.wake();
+            messageReady.sleep();
+        }
+
+        if (!mutex.isHeldByCurrentThread()) {
+            mutex.acquire();
+        }
+
+        waitingListeners--;
+        int return_message = this.currentMessage;
+        validMessage = false;
+        
+        mutex.release();
+        return return_message;
     }
 }
